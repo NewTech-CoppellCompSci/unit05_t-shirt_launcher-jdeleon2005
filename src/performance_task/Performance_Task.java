428e64bef3b8ee4df058c343b1329b41be5b96f6
jdeleon2005 <jdeleon@newtech.coppellisd.com>
2023-01-17 10:42:44 -0600

    Update Performance_Task.java

--- src/performance_task/Performance_Task.java
+++ src/performance_task/Performance_Task.java
@@ -1,10 +1,37 @@
 package performance_task;
 
+import java.util.Scanner;
+
 public class Performance_Task {
 
+	public static double launchVelocity;
+    public static double launchAngle;
+    public static double time = 0;
+    public static double xPos;
+    static double yPos;
+    
 	public static void main(String[] args) {
-		
-		
+	Scanner inKey = new Scanner(System.in);
+            System.out.println("What is the T-Shirt launcher's initial velocity? (m/s)");
+            launchVelocity = inKey.nextDouble();
+            System.out.println("What is the T-Shirt launcher's an launch angle? (degrees)");
+            launchAngle = inKey.nextDouble();
+            while (time < 100) {
+                xPos = Math.cos(Math.toRadians(launchAngle)) * time * launchVelocity;
+                yPos = Math.sin(Math.toRadians(launchAngle)) * time * launchVelocity - 0.5 * 9.8 * Math.pow(time, 2);
+                if(yPos < 0) {
+                    yPos = 0;
+                }
+                System.out.println("Time: " + time + "s");
+                System.out.println("The X-Position is: " + xPos + "m");
+                System.out.println("The Y-Position is: " + yPos + "m");
+                if(yPos == 0 && time > 0){
+                    break;
+                }
+                time++;
+            }
+        }
+
 		
 		
 		
@@ -12,4 +39,4 @@ public class Performance_Task {
 		
 	}
 	
-}
+
